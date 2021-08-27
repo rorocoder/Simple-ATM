ac5d0468368d8e88ddd382563b3178a25fb40821
Rohan M <rohan0866@gmail.com>
2021-08-27 09:15:53 -0700

    I made the whole thing work

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,13 @@
-
+import java.util.*;
 public class ATM {
-
-}
+	HashMap<String, Double> accounts = new HashMap<String, Double>();
+	
+	public void deposit(String bankID, Double amount) {
+		if(accounts.containsKey(bankID)) {
+			accounts.put(bankID, accounts.get(bankID) + amount);
+		}
+		else {
+			accounts.put(bankID, amount);
+		}
+	}
+}	
